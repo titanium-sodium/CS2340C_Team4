91516f49fe90361f1bde82b1a69ea756e73e91b3
Julian McCorkle <julianmoet1@gmail.com>
2024-10-01 20:18:47 -0400

    Updated android manifest to have splash page as default starting activity. Added basic content for splash page. Need to add the buttons that take the user to alternate pages.

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/views/SplashPage.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/views/SplashPage.java
@@ -7,4 +7,9 @@ import android.os.Bundle;
 import com.example.sprintproject.R;
 
 public class SplashPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_page);
+    }
 }
