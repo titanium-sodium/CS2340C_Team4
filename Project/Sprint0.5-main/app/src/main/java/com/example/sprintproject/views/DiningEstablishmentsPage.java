689259eb7da80a4b589d3aa4ea048279a64065a0
andresquast <andresquast@gmail.com>
2024-10-03 17:51:04 -0400

    Created all of the placeholder pages (Logistics Screen, Destination Screen, Dining Establishment Screen, Accommodations Screen, Transportation Screen, Community Engagement Screen), including the XML and java files.

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/views/DiningEstablishmentsPage.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/views/DiningEstablishmentsPage.java
@@ -1,8 +1,14 @@
 package com.example.sprintproject.views;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.example.sprintproject.R;
+
 public class DiningEstablishmentsPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dining_establishments_screen);
+    }
 }
