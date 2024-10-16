35d02bfcca286a5f24956becebea26de7700d3cd
Julian McCorkle <julianmoet1@gmail.com>
2024-10-15 21:13:02 -0400

    Created java files for Project, Task, and Teammember in SOLID and GRASP.

---
+++ SOLID_GRASP/TeamMember.java
@@ -0,0 +1,9 @@
+class TeamMember {
+    private String name;
+    private String email;
+    private String role;
+
+    public TeamMember (String name, String email, String role) {
+        //instantiate variables.
+    }
+}
