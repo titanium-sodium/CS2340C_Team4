47b4025112cc21a0cbe35938d8c2654fbd2fd06d
andresquast <andresquast@gmail.com>
2024-10-26 17:32:47 -0400

    Progress on LogisticsPage and getting closer to having it update in live time.

---
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/model/TravelStats.java
@@ -0,0 +1,19 @@
+package com.example.sprintproject.model;
+
+public class TravelStats {
+    private int allottedDays;
+    private int plannedDays;
+
+    public TravelStats(int allottedDays, int plannedDays) {
+        this.allottedDays = allottedDays;
+        this.plannedDays = plannedDays;
+    }
+
+    public int getAllottedDays() {
+        return allottedDays;
+    }
+
+    public int getPlannedDays() {
+        return plannedDays;
+    }
+}
