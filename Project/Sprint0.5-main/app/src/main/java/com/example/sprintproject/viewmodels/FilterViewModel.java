db919749940a2bbf349ea31d4b7d3153812098fd
andresquast <andresquast@gmail.com>
2024-11-07 18:19:51 -0500

    Filter!!! (but added files i forgor)

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/viewmodels/FilterViewModel.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/viewmodels/FilterViewModel.java
@@ -1,20 +1,48 @@
 package com.example.sprintproject.viewmodels;
 
+import com.example.sprintproject.model.FiltersModel;
+import com.example.sprintproject.model.DiningFilterModel;
+import com.example.sprintproject.model.AccommodationsFilterModel;
 
 public class FilterViewModel {
     private boolean filter;
     private String type;
-    public FilterViewModel(boolean filter, String type) {
+    private DiningReservationViewModel diningViewModel;
+    private AccommodationsViewModel accommodationsViewModel;
+    private FiltersModel filterModel;
+
+    public FilterViewModel(boolean filter, String type, Object viewModel) {
         this.filter = filter;
         this.type = type;
+
+        // Initialize the appropriate filter model and viewModel based on type
+        if (type.equals("Dining")) {
+            this.diningViewModel = (DiningReservationViewModel) viewModel;
+            this.filterModel = new DiningFilterModel();
+        } else if (type.equals("Accommodations")) {
+            this.accommodationsViewModel = (AccommodationsViewModel) viewModel;
+            this.filterModel = new AccommodationsFilterModel();
+        }
     }
 
-    public void changeFilter(boolean filter, String type) {
-        //TODO
+    public void changeFilter(boolean currentFilter, String filterType) {
+        if (filterType.equals(this.type)) {
+            // Get the new filter state from the model
+            this.filter = filterModel.changeFilter(filterType);
+
+            // Apply sorting using the model's sort field
+            if (type.equals("Dining") && diningViewModel != null) {
+                diningViewModel.setSortOrder(this.filter, filterModel.getSortField());
+            } else if (type.equals("Accommodations") && accommodationsViewModel != null) {
+                accommodationsViewModel.setSortOrder(this.filter, filterModel.getSortField());
+            }
+        }
     }
-    public boolean getFilter()  {
+
+    public boolean getFilter() {
         return filter;
     }
+
     public String getType() {
         return type;
     }
